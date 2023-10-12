dd25ec3b743a07f3da0d14c64c8e032f17b32ef7
= <=>
2023-10-12 11:18:30 -0300

    primer commit

---
+++ tasks/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Task
+
+
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", )
+
+
+admin.site.register(Task, TaskAdmin)
+
+
+
+
+
